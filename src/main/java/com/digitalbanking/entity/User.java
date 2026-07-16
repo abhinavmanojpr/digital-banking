8d885473c6519437a6a742ab9b036b2c8aaf5d43
Abhinav M <abhinavmanojpr@gmail.com>
2026-07-16 12:58:32 +0530

    Implement the customer entity and one-to-one relationship

--- src/main/java/com/digitalbanking/entity/User.java
+++ src/main/java/com/digitalbanking/entity/User.java
@@ -1,4 +1,13 @@
 package com.digitalbanking.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.digitalbanking.enums.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
@@ -14,5 +23,21 @@ public class User {
 
     @Column(nullable=false, unique=true)  
     private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
     
 }
